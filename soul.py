5364a631679372798c127690db04a6f37e6819dd
Sona22890 <sonakanwar8302@gmail.com>
2024-12-14 11:04:54 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -367,7 +367,7 @@ Official Channel :- https://t.me/+kRKyJDJYT_djZjZl
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     user_name = message.from_user.first_name
-    response = f'''🔥Gyrange ke LODE pe aapka swagat hai, {user_name}! Sabse acche se bgmi ki maa behen yahi hack karta hai. Kharidne ke liye Kira se sampark karein.
+    response = f'''🔥SOMS ke LODE pe aapka swagat hai, {user_name}! Sabse acche se bgmi ki maa behen yahi hack karta hai. Kharidne ke liye Kira se sampark karein.
 🤗Try To Run This Command : /help 
 💵BUY :-@SomsPvtt'''
     bot.reply_to(message, response)
